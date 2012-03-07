db0600ea245f864416a25f9583e9b9a02af411fa
Konforti <konforti@gmail.com>
2012-03-07 20:30:21 +0200

    charts

--- profiles/openideal/modules/custom/ideal_chart/theme/ideal_chart.js
+++ profiles/openideal/modules/custom/ideal_chart/theme/ideal_chart.js
@@ -4,50 +4,52 @@
  */
 
 // Load the Visualization API and the chart package.
-google.load("visualization", "1", {packages:["corechart", "gauge"]});
+google.load("visualization", "1", {packages:["corechart", "columnchart", "piechart", "gauge"]});
 
 (function($) {
   Drupal.behaviors.idealChart = {
     attach: function(context, settings) {
-      // Set a callback to run when the Google 
-      // Visualization API is loaded.
       google.setOnLoadCallback(drawChart);
-
       // Callback that creates and populates a data table, 
       // instantiates the chart, passes in the data and
       // draws it.
       function drawChart() {
-        var row = new Array();
-        // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Label');
-        
-        // Adding the colomns. 
-        // These are graphs titles.
-        for (var col in settings.columns) {
-          data.addColumn('number', settings.columns[col]);
-        }
-
-        // Adding the heders.
-        // The rows titles.
-        for (var i in settings.header) {
+        // Loop on the charts in the settings.
+        for (var chartId in settings.chart) {
           var row = new Array();
-          // Adding the rows.
-          // The points of the column for each row.
-          for (var j in settings.rows) {
-             row[j] = parseInt(settings.rows[j][i]);
-          } 
-          row.unshift(settings.header[i]);
-          data.addRows([row])
-        };
+            // Create the data table.
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'Label');
 
-        // Set chart options
-        var options = settings.options;
+            // Adding the colomns. 
+            // These are graphs titles.
+            for (var col in settings.chart[chartId].columns) {
+              data.addColumn('number', settings.chart[chartId].columns[col]);
+            }
 
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization[settings.type](document.getElementById(settings.html_id));
-        chart.draw(data, options);
-      }
+            // Adding the heders.
+            // The rows titles.
+            for (var i in settings.chart[chartId].header) {
+              var row = new Array();
+              // Adding the rows.
+              // The points of the column for each row.
+              for (var j in settings.chart[chartId].rows) {
+                row[j] = parseInt(settings.chart[chartId].rows[j][i]);
+              } 
+              row.unshift(settings.chart[chartId].header[i]);
+              data.addRows([row])
+            };
+
+            // Set chart options
+            var options = settings.chart[chartId].options;
+
+            // Instantiate and draw our chart, passing in some options.
+            var chart = new Object;
+            chart[settings.chart[chartId]] = new google.visualization[settings.chart[chartId].chartType](document.getElementById(settings.chart[chartId].containerId));
+            chart[settings.chart[chartId]].draw(data, options);
+          }
+        }
+        
     }
   };
 })(jQuery);
