f76329da16ef438b326d29c098a5b5063d47eafb
Konforti <konforti@gmail.com>
2012-03-10 14:00:15 +0200

    analystics dashboard

--- profiles/openideal/modules/custom/google_chart_tools/theme/google_chart_tools.js
+++ profiles/openideal/modules/custom/google_chart_tools/theme/google_chart_tools.js
@@ -16,40 +16,42 @@ google.load("visualization", "1", {packages:["corechart", "gauge"]});
       function drawChart() {
         // Loop on the charts in the settings.
         for (var chartId in settings.chart) {
-          var row = new Array();
-            // Create the data table.
-            var data = new google.visualization.DataTable();
-            data.addColumn('string', 'Label');
+        var row = new Array();
+          // Create the data table.
+          var data = new google.visualization.DataTable();
+          data.addColumn('string', 'Label');
 
-            // Adding the colomns. 
-            // These are graphs titles.
-            for (var col in settings.chart[chartId].columns) {
-              data.addColumn('number', settings.chart[chartId].columns[col]);
-            }
+          // Adding the colomns. 
+          // These are graphs titles.
+          for (var col in settings.chart[chartId].columns) {
+            data.addColumn('number', settings.chart[chartId].columns[col]);
+          }
 
-            // Adding the heders.
-            // The rows titles.
-            for (var i in settings.chart[chartId].header) {
-              var row = new Array();
-              // Adding the rows.
-              // The points of the column for each row.
-              for (var j in settings.chart[chartId].rows) {
-                row[j] = parseInt(settings.chart[chartId].rows[j][i]);
-              } 
-              row.unshift(settings.chart[chartId].header[i]);
-              data.addRows([row])
-            };
+          // Adding the heders.
+          // The rows titles.
+          for (var i in settings.chart[chartId].header) {
+            var row = new Array();
+            // Adding the rows.
+            // The points of the column for each row.
+            for (var j in settings.chart[chartId].rows) {
+              row[j] = parseInt(settings.chart[chartId].rows[j][i]);
+            } 
+            row.unshift(settings.chart[chartId].header[i]);
+            data.addRows([row])
+          };
 
-            // Set chart options
-            var options = settings.chart[chartId].options;
+          // Set chart options
+          var options = settings.chart[chartId].options;
 
-            // Instantiate and draw our chart, passing in some options.
-            var chart = new Object;
-            chart[settings.chart[chartId]] = new google.visualization[settings.chart[chartId].chartType](document.getElementById(settings.chart[chartId].containerId));
+          // Instantiate and draw our chart, passing in some options.
+          var chart = new Object;
+          var element = document.getElementById(settings.chart[chartId].containerId);
+          if (element) {
+            chart[settings.chart[chartId]] = new google.visualization[settings.chart[chartId].chartType](element);
             chart[settings.chart[chartId]].draw(data, options);
           }
         }
-        
+      }   
     }
   };
 })(jQuery);
