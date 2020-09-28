9946429f81f1575e023406c2b54d1cb4e07735f5
Nazariy Velychenko <n.velychenko@dev-branch.com>
2020-09-28 15:25:44 +0300

    OI-243,254 Implemented logic to change default format of input html5 date formatter into "dd-mm-yyyy", installed the momentjs library, for it. Changed format field_created description of every node types. Implemented logic to validate minimal value possible to set into schedule open/close.

--- modules/openideal_challenge/js/schedule.js
+++ modules/openideal_challenge/js/schedule.js
@@ -18,17 +18,36 @@
       $('.challenge-schedule-local-machine-time').once('openideal_challenge_schedule').each(function () {
         // Format the date.
         var date = new Date();
-        const year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date)
-        const month = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(date)
-        const day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date)
-        var localFormattedDate = `${year}-${month}-${day}`;
+        const year = new Intl.DateTimeFormat('en', {year: 'numeric'}).format(date)
+        const month = new Intl.DateTimeFormat('en', {month: '2-digit'}).format(date)
+        const day = new Intl.DateTimeFormat('en', {day: '2-digit'}).format(date)
+        var localFormattedDate = `${day}/${month}/${year}`;
         $(this).html(Drupal.t('Your time is: @time. <br/> Leave blank to use the time of submission ("Now").',
-          {
-            '@time': localFormattedDate,
-          })
+        {
+          '@time': localFormattedDate,
+        })
         )
       })
     }
   };
+
+  /**
+   * Change html5 date input view format.
+   *
+   * @type {Drupal~behavior}
+   *
+   * @prop {Drupal~behaviorAttach} attach
+   *   Attach dynamic format change to date.
+   */
+  Drupal.behaviors.openidealChallengeHMTL5Date = {
+    attach: function () {
+      $('input[type="date"]').once('openideal_challenge_html5_date').each(function () {
+        $(this).on('change', function () {
+          var attribute = this.value == '' ? 'dd/mm/yyyy' : moment(this.value, 'YYYY-MM-DD').format(this.getAttribute('data-date-format'))
+          this.setAttribute('data-date', attribute);
+        }).trigger("change")
+      })
+    }
+  }
 }
 )(jQuery, Drupal);
